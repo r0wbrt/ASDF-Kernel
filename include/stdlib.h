12d7b4b0cc24c0c025a4a23da7d60540538fa197
Robert Christian Taylor <r0wbrt@gmail.com>
2010-06-14 21:15:21 -0400

    kernel-tree

--- include/stdlib.h
+++ include/stdlib.h
@@ -1,4 +1,28 @@
-/*Copyright 2010 Robert Christian Taylor. All Rights Reserved.*/
+/*
+###############################################################################################
+# Copyright 2010 Robert Christian Taylor. All rights reserved.                                #
+#                                                                                             #
+# Redistribution and use in source and binary forms, with or without modification, are        #
+# permitted provided that the following conditions are met:                                   #
+#                                                                                             #
+#  1. Redistributions of source code must retain the above copyright notice, this list of     #
+#     conditions and the following disclaimer.                                                #
+#                                                                                             #
+# THIS SOFTWARE IS PROVIDED BY Robert Christian Taylor ``AS IS'' AND ANY EXPRESS OR IMPLIED   #
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND    #
+# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Robert Christian Taylor  #
+# OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR      #
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR    #
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON    #
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING          #
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF        #
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                                                  #
+#                                                                                             #
+# The views and conclusions contained in the software and documentation are those of the      #
+# authors and should not be interpreted as representing official policies, either expressed   #
+# or implied, of Robert Christain Taylor.                                                     #
+###############################################################################################
+*/
 #ifndef _STDLIB_H_
 #define _STDLIB_H
 #ifndef NULL
